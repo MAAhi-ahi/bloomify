58a8b03a19cdd7a125e55f4ddd17548f933cd76a
MA Ahi <bijoyrashidul21@gmail.com>
2025-02-17 16:21:56 +0600

    update

--- bloomify-fastfoods/src/services/apiOrder.js
+++ bloomify-fastfoods/src/services/apiOrder.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000'; 
+const API_BASE_URL = 'http://localhost:5001'; 
 export async function createOrder(newOrder) {
   try {
     const res = await fetch(`${API_BASE_URL}/api/order`, {
